0452b75b9039ccca7afdbb2c479e57d8980bd124
kayes55 <kayes.shamil@gmail.com>
2019-01-03 23:44:25 +0600

    Added NewVC and some basic UI fixes for dev branch

---
+++ TestGit/TestGit/NewVC.swift
@@ -0,0 +1,31 @@
+//
+//  NewVC.swift
+//  TestGit
+//
+//  Created by Imrul Kayes on 1/3/19.
+//  Copyright © 2019 Imrul Kayes. All rights reserved.
+//
+
+import UIKit
+
+class NewVC: UIViewController {
+
+    @IBOutlet weak var nameLabel: UILabel!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
